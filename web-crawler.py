48a608f04969a4c28d9a2606b6ea36426bdd023f
Neacsu Radu <radu-stefan.neacsu@my.fmi.unibuc.ro>
2016-07-04 12:56:11 +0300

    Update web-crawler.py
    am sortat problemele de pe infoarena in functie de ratingul lor

--- web-crawler.py
+++ web-crawler.py
@@ -1,9 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
-
+import re
+import operator
 file_links = open( 'links.txt', 'w' )
-
-
+dictionar = { }
 def function ( number_of_pages ):
     page_number = 0
     while page_number < number_of_pages:
@@ -11,8 +11,23 @@ def function ( number_of_pages ):
         source_code = requests.get( url )
         plain_text = source_code.text
         soup = BeautifulSoup( plain_text )
-        for link in soup.findAll( 'td', { 'class':'task'} ):
-            href = "http://www.infoarena.ro" + link.find('a').get('href')
-            file_links.write( href + '\n' )
+        for link in soup.findAll( 'tr', { 'class':re.compile("odd|even")  } ):
+            task = link.find( 'td', { 'class':'task' } ) # aici avem linkul  problemei
+            task_link = task.find( 'a' )  # aici avem linkul problemei
+            href = "http://www.infoarena.ro" + task_link.get('href')
+
+            rating = link.find( 'td', { 'class':'rating' } ) # aici avem ratingul pe care urmeaza sa il evaluam
+
+            if rating.find( 'div', { 'class':'hidden'}) is not None :
+                rating2 = rating.find( 'div', { 'class':'hidden'} )
+                value = rating2.string
+                dictionar.update( { href:value[0] } )
+            else :
+                dictionar.update( { href:'0'})
         page_number += 1
+
 function( 36 )
+sdict = sorted( dictionar.items(), key = operator.itemgetter(1) )
+for number, letter in sdict:
+    file_links.write("\n".join(["%s %s" % (number, letter)]) + "\n")
+file_links.close()
